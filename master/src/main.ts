024afc59f656738d9d8cf83dc4f4d30ab475a276
Matheus-Mazepa <mfmazepa@hotmail.com>
2024-10-01 01:21:05 -0300

    feat: add module node

---
+++ master/src/main.ts
@@ -0,0 +1,28 @@
+import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
+import { AppModule } from './app.module';
+import {Partitioners} from "kafkajs";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: process.env.MASTER_KAFKA_CLIENT_ID,
+        brokers: [process.env.KAFKA_BROKER]
+      },
+      consumer: {
+        groupId: process.env.MASTER_KAFKA_GROUP_ID,
+      },
+      producer: {
+        createPartitioner: Partitioners.LegacyPartitioner,
+      },
+    },
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(3000);
+}
+bootstrap();
